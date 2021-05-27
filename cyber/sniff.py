6ad5c1e89e9c76112fb8c42eeb555a3bbee5d0bb
Yanai Shaviv <yanaishavivra@gmail.com>
2021-05-28 00:17:15 +0300

    sniffing added

--- cyber/sniff.py
+++ cyber/sniff.py
@@ -1,13 +1,14 @@
 import scapy.all as scapy
+import constants
 
 def check(packet):
     if (scapy.IP in packet):
-        return packet[scapy.IP].dst != '192.168.68.108' 
+        return packet[scapy.IP].dst == constants.SERVER_IP and packet[scapy.IP].src == constants.CLIENT_IP  
     return False
 
 
 
 # packets = scapy.all.sniff(2)
 packets = 3
-yes = scapy.sniff(count = 10, lfilter = check)
-print('success ', yes[0].show())
+yes = scapy.sniff(count = constants.NUMBER_OF_PACKETS, lfilter = check)
+print('success ', yes[2][scapy.RAW].load)
